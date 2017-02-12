0ae4f6a805bc553b268a2c2d0b94bcc6730f9ff5
James Gosbell <jamesgosbell@gmail.com>
2017-02-12 14:00:00 +0100

    Refactored mutli check code

--- 10 - Hold Shift and Check Checkboxes/script.js
+++ 10 - Hold Shift and Check Checkboxes/script.js
@@ -1,20 +1,16 @@
-const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+const checkboxes = Array.from(document.querySelectorAll('.inbox input[type="checkbox"]'));
 
 let lastChecked;
 
-function handleMultiCheck(e){
-  if(e.shiftKey && this.checked) {
-    let inBetween = false;
-    checkboxes.forEach(checkbox => {
-      if(checkbox === this || checkbox === lastChecked){
-        inBetween = !inBetween;
-      }
-      if (inBetween) {
-        checkbox.checked = true;
-      }
-    });
-  }
-  lastChecked = this;
+function handleMultiCheck(e) {
+    if (e.shiftKey && this.checked) {
+        const startCheckIndex = checkboxes.indexOf(lastChecked);
+        const endCheckIndex = checkboxes.indexOf(this);
+        const inBetweenChecks = checkboxes.filter(
+          (checkbox, index) => index > startCheckIndex && index < endCheckIndex);
+        inBetweenChecks.forEach(checkbox => checkbox.checked = true);
+    }
+    lastChecked = this;
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleMultiCheck));
