a987d85d284e560b43dc935f757c3d5de1dc190d
James Gosbell <jamesgosbell@gmail.com>
2017-02-15 15:15:11 +1100

    Completed Day 12 Key Sequence Detection Unicorn exercise

---
+++ 12 - Key Sequence Detection/script.js
@@ -0,0 +1,12 @@
+const pressed = [];
+const secretCode = 'unicorn'
+window.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  pressed.push(e.key);
+  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+  if (pressed.join('').includes(secretCode)) {
+      console.log('DING! DING!');
+      cornify_add();
+  }
+  console.log(pressed);
+});
